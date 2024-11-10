272a93b6956d0901ff3278fb53d1a492c5a3ed50
Reful4ent <dima2323fr3@gmail.com>
2024-11-10 23:45:07 +0700

    try

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path('category/<slug:category_slug>/', views.category_posts,
          name='category_post'),
+    path('posts/<int:id>/', views.post_detail, name='post_detail'),
 ]
--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'pages'
 
-
 urlpatterns = [
-    path('pages/about/', views.about, name='about'),
-    path('pages/rules/', views.rules, name='rules'),
+    path('about/', views.about, name='about'),
+    path('rules/', views.rules, name='rules'),
 ]
